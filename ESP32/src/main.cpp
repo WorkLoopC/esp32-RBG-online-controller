77c665caab0562ef2d80f07223f72ce8709b5f01
Jan Kozel <153216339+WorkLoopC@users.noreply.github.com>
2024-02-07 14:46:17 +0100

    Update main.cpp

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -1,4 +1,3 @@
-//Working on finishing this code
 #include <Arduino.h>
 #include <WiFiClientSecure.h>
 
@@ -29,13 +28,12 @@ const long timeoutTime = 2000;
 
 void setup(){
 Serial.begin(115200);
-ledcSetup(redCh, freq, res);
-ledcSetup(greenCh, freq, res);
-ledcSetup(blueCh, freq, res);
 pinMode(red, OUTPUT);
 pinMode(green, OUTPUT);  
 pinMode(blue, OUTPUT);       
-Serial.println();Serial.println();
+ledcSetup(redCh, freq, res);
+ledcSetup(greenCh, freq, res);
+ledcSetup(blueCh, freq, res);
 Serial.print("Connecting to ");
 Serial.println(ssid);
 WiFi.begin(ssid, password);
@@ -49,6 +47,59 @@ Serial.println("IP address: ");
 Serial.println(WiFi.localIP());
 server.begin();
 }
+
 void loop(){
-WiFiClient client = server.available();   
+WiFiClient client = server.available();
+if (client) {                             
+    currentTime;
+    previousTime = currentTime;
+    Serial.println("New Client.");          
+    String currentLine = "";                
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {            
+      currentTime = millis();
+      if (client.available()) {             
+        char c = client.read();             
+        Serial.write(c);                    
+        header += c;
+        if (c == '\n') {                    
+          if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            client.println("<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\">");
+            client.println("<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jscolor/2.0.4/jscolor.min.js\"></script>");
+            client.println("</head><body><div class=\"container\"><div class=\"row\"><h1>ESP Color Picker</h1></div>");
+            client.println("<a class=\"btn btn-primary btn-lg\" href=\"#\" id=\"change_color\" role=\"button\">Change Color</a> ");
+            client.println("<input class=\"jscolor {onFineChange:'update(this)'}\" id=\"rgb\"></div>");
+            client.println("<script>function update(picker) {document.getElementById('rgb').innerHTML = Math.round(picker.rgb[0]) + ', ' +  Math.round(picker.rgb[1]) + ', ' + Math.round(picker.rgb[2]);");
+            client.println("document.getElementById(\"change_color\").href=\"?r\" + Math.round(picker.rgb[0]) + \"g\" +  Math.round(picker.rgb[1]) + \"b\" + Math.round(picker.rgb[2]) + \"&\";}</script></body></html>");
+            client.println();
+            if(header.indexOf("GET /?r") >= 0) {
+              pos1 = header.indexOf('r');
+              pos2 = header.indexOf('g');
+              pos3 = header.indexOf('b');
+              pos4 = header.indexOf('&');
+              redString = header.substring(pos1+1, pos2);
+              greenString = header.substring(pos2+1, pos3);
+              blueString = header.substring(pos3+1, pos4);
+              ledcWrite(red, redString.toInt());
+              ledcWrite(green, greenString.toInt());
+              ledcWrite(blue, blueString.toInt());
+            }
+            break;
+          } else { 
+            currentLine = "";
+          }
+        }
+      }
+    }
+    header = "";
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
 }
